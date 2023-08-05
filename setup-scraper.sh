51f88800fb2db538ae94c5fbf1f2894170540b99
Ubuntu <ubuntu@ip-172-31-21-200.ec2.internal>
2023-08-05 14:08:49 +0000

    setup scraper

---
+++ setup-scraper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get install -y google-chrome-stable
+apt-get install -y qq unzip
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
